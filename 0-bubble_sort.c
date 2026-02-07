6a977982354cc5d5631bafd821e91994996e8049
Your Name <you@example.com>
2026-02-07 17:02:42 +0000

    add

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sorts.h"
+/**
+ * bubble_sort - function that sorts array list
+ * @array:array
+ * @size:size of the array
+ * Return:prints array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
