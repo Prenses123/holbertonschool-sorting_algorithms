87a2968e7427f3e5f3e64855083000f1b6929eae
Prenses123 <137319134+Prenses123@users.noreply.github.com>
2026-02-07 21:08:36 +0400

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,17 +7,18 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp;
+	int temp;
+	size_t i, j;
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
